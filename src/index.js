29b9410570d8e09e4e11f48b85fe87bf57877bc4
Emil Kloeden <emilkloeden@gmail.com>
2017-01-23 23:08:46 +1030

    test(error handling): Added tests for numberOfSpaces <0 or >8

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ module.exports = function(str, numberOfSpaces=2) {
     if (numberOfSpaces < 0 || numberOfSpaces > 8) {
         throw new RangeError('`tabs-to-spaces-stream` expects to replace each tab with 0-8 spaces. If the numberOfSpaces argument is not passed, 2 spaces will be used.')
     }
-    let space = ''
-    for (let i = 0; i < numberOfSpaces; i++) {
-        space += ' '
+    
+    const intSpaces = Math.trunc(numberOfSpaces)
+    let spaces = ''
+
+    for (let i = 0; i < intSpaces; i++) {
+        spaces += ' '
     }
-    return str.replace(/\t/g, space)
+
+    return str.replace(/\t/g, spaces)
 }
--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,25 @@ const convert = require('../src/index')
 
 describe('tabs-to-spaces-stream', () => {
     describe('tabs-to-spaces', () => {
+        it('should throw a RangeError if number of spaces requested is less than 0', () => {
+            expect(function() {
+                // to.throw requires a function
+                convert('str', -1)
+            }).to.throw(RangeError)
+        })
+        it('should throw a RangeError if number of spaces requested is greater than 8', () => {
+            expect(function() {
+                // to.throw requires a function
+                convert('str', -1)
+            }).to.throw(RangeError)
+        })
+        it('should truncate non-integer number of spaces to an integer', () => {
+            const beforeString = '\thello'
+            const afterStringFour = convert(beforeString, 4)
+            const afterStringFourPointFive  = convert(beforeString, 4.5)
+
+            expect(afterStringFourPointFive).to.equal(afterStringFour)
+        })
         it('should convert tabs to spaces', () => {
             const beforeString = '\thello'
             const afterString = '  hello'
