c9d009f22087607b01fd068e2fc819b8bfc110af
Emil Kloeden <emilkloeden@gmail.com>
2017-01-24 22:11:52 +1030

    feat(streams): Added convertStream method
    module.exports = convert is now module.exports = {convert, convertStream}
    
    BREAKING CHANGES convert must now be pulled off of the import e.g. {convert}

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-module.exports = function(str, numberOfSpaces=2) {
+const {Transform} = require('stream')
+
+function convertStream(numberOfSpaces=2) {
+    const myTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            chunk = chunk.toString('utf8')
+            this.push(convert(chunk, numberOfSpaces))
+            callback()
+        }
+    })
+    return myTransform
+}
+
+
+function convert(str, numberOfSpaces=2) {
+    if (typeof str !== 'string') {
+        str = str.toString()
+    }
+
     if (numberOfSpaces < 0 || numberOfSpaces > 8) {
         throw new RangeError('`tabs-to-spaces-stream` expects to replace each tab with 0-8 spaces. If the numberOfSpaces argument is not passed, 2 spaces will be used.')
     }
@@ -12,3 +30,5 @@ module.exports = function(str, numberOfSpaces=2) {
 
     return str.replace(/\t/g, spaces)
 }
+
+module.exports = {convert, convertStream};
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,29 @@
+const fs = require('fs')
+const StreamTest = require('streamtest')
 const {expect} = require('chai')
-const convert = require('../src/index')
+const {convert, convertStream} = require('../src/index')
+
+
+const noTabs = str => (str.indexOf('\t') === -1)
 
 describe('tabs-to-spaces-stream', () => {
-    describe('tabs-to-spaces', () => {
+    describe('convertStream function', () => {
+        it('replace all tabs with spaces over a stream', done => {
+            const readStream = fs.createReadStream('./test/tabbed-out-text-file.txt')
+            readStream
+                .pipe(convertStream(2))
+                .pipe(StreamTest['v1'].toText(function(err, text){
+                    if(err) {
+                        done(err)                   
+                    } 
+                    else {
+                        expect(text).to.satisfy(noTabs)
+                        done()
+                    }
+               }))
+            })
+    })
+    describe('convert function', () => {
         it('should throw a RangeError if number of spaces requested is less than 0', () => {
             expect(function() {
                 // to.throw requires a function
@@ -32,7 +53,6 @@ describe('tabs-to-spaces-stream', () => {
         it('should remove all tabs from a document', () => {
             const beforeString = '\tWorld\t\t'
             const convertedString = convert(beforeString)
-            const noTabs = str => (str.indexOf('\t') === -1)
             
             expect(convertedString).to.satisfy(noTabs)
         })
